8c7fc2f30e275427aef5b89577ab2abe4cde5f83
qeury <web.macsim@yandex.ru>
2020-09-12 01:46:27 +0700

    added section create

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
-$(function(){
+let select = function(){
+  let selectHeader = document.querySelectorAll
+  (selectors '.select__header');
 
+  selectHeader.forEach(callbackfn: item=>{
+      item.addEventListener('click', listener: function(){
+        this.parentElement.classList.toggle
+        (token: 'is-active');
 
-  
+      })
+  })
+};
 
-});
+select();
