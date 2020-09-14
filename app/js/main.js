6e41519b78b16c36253c9b04fd382298543854eb
qeury <web.macsim@yandex.ru>
2020-09-15 02:01:39 +0700

    added newest products

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,1 @@
-let select = function(){
-  let selectHeader = document.querySelectorAll
-  (selectors '.select__header');
 
-  selectHeader.forEach(callbackfn: item=>{
-      item.addEventListener('click', listener: function(){
-        this.parentElement.classList.toggle
-        (token: 'is-active');
-
-      })
-  })
-};
-
-select();
