fb6b3fa51824ef03717bd8c8d5982359667ea49d
qeury <web.macsim@yandex.ru>
2020-09-23 01:35:14 +0700

    upgrade

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
+$(function() {
 
+  $(".card__star").rateYo({
+    rating: 5,
+    spacing: "3px",
+    starWidth: "15px"
+  });
+
+  $('.week-prod__card').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/right.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/left.png" alt=""></button>'
+  });
+
+  let mixer = mixitup('.new-prod__card');
+
+});
